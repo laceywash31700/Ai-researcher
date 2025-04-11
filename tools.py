560d206fd7ab71d3d238a8c900fb4f206d6d009b
Lacey W. <lacey.m.washington@gmail.com>
2025-04-11 18:28:15 -0500

    added tools and constraints files that takes papers from arxiv and converts the pdf to text as well as set up constaints for openai key and llm

---
+++ tools.py
@@ -0,0 +1,44 @@
+import arxiv
+import requests
+import os
+
+client = arxiv.Client()
+
+
+def fetch_arxiv_papers(title: str, paper_count: int):
+    search_query = f'all:"{title}"'
+    search = arxiv.Search(
+        query=search_query,
+        max_results=paper_count,
+        sort_by=arxiv.SortCriterion.SubmittedDate,
+    )
+    papers = []
+    results = client.results(search)
+    for result in results:
+        paper_info = {
+          "title": result.title, 
+          "summary": result.summary, 
+          "published": result.published, 
+          "journal_ref": result.journal_ref,
+          "doi": result.doi,
+          "primary_category": result.primary_category,
+          "category": result.category,
+          "pdf_url": result.pdf_url,
+          "arxiv_url": result.entry_id,
+          "authors": [author.name for author in result.authors]
+          }
+        papers.append(paper_info)
+    return papers
+  
+  
+def download_pdf(pdf_url: str, output_file_name: str):
+      try:
+        os.makedirs("papers", exist_ok=True)
+        full_output_path = os.path.join("papers", output_file_name)
+        response = requests.get(pdf_url)
+        response.raise_for_status()
+        with open(full_output_path, "wb") as file:
+          file.write(response.content)
+        return f"PDF saved as {full_output_path}"
+      except requests.exceptions.RequestException as e:
+       return f"Error downloading PDF: {e}"
